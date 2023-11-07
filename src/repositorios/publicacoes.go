5efd83c30b01a275d27c0611af5c887a994eb731
oLucasPopov <lucaspopov@protonmail.com>
2023-11-07 20:23:24 -0300

    feat: create method for liking posts

--- src/repositorios/publicacoes.go
+++ src/repositorios/publicacoes.go
@@ -176,3 +176,17 @@ func (repositorio Publicacoes) BuscarPorUsuario(usuarioID uint64) ([]modelos.Pub
 
 	return publicacoes, nil
 }
+
+func (repositorio Publicacoes) Curtir(publicacaoID uint64) error {
+	statement, erro := repositorio.db.Prepare("update publicacoes set curtidas = curtidas + 1 where id = $1")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
